5c56393ff92726db757a2950a25d809f44953c41
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2020-12-11 23:09:08 +0200

    Delete CTestTestfile.cmake

--- build/Engine/ThirdParty/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/i-use-gentoo-btw/Desktop/UntitledVulkanGameEngine/Engine/ThirdParty/glm/glm
-# Build directory: /home/i-use-gentoo-btw/Desktop/UntitledVulkanGameEngine/build/Engine/ThirdParty/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
