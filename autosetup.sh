7fe3ddaae49d897d1deff6cca7702a8ad9d5b007
Madman10K <contact@madman10k.online>
2021-02-23 20:45:44 +0200

    Added autosetup script

---
+++ autosetup.sh
@@ -0,0 +1,18 @@
+echo "Compiling GLEW ..."
+
+cd Engine/ThirdParty/glew/auto && make && cd .. && make && cd ../../../
+
+echo "Compiled GLEW!"
+
+echo "Creating project file with default settings ..."
+
+touch uvproj.yaml && echo "\
+name: Game
+renderer-subsystem: Legacy
+version: 1.0.0
+engine-version: 1.0.0
+production: false" > uvproj.yaml
+
+echo "Compiling build tool and generating files ..."
+
+cd UVKBuildTool/ && mkdir build && cd build && cmake .. -G "Unix Makefiles" && make && ./UVKBuildTool --generate
