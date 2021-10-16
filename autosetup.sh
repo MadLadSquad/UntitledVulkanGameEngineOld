e1669a9fdc958858dc9f27ca3f189b3d0bbfa6cb
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-10-16 14:49:29 +0300

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -43,7 +43,7 @@ echo " "
 cd UVKBuildTool/ || exit
 mkdir build || exit # Will store our compiled binary
 cd build || exit
-cmake .. || exit # Generate the UVKBuildTool project files
+cmake .. -G "Visual Studio 16 2019" || exit # Generate the UVKBuildTool project files
 
 # Try to run MSBuild first, if it fails we are either on a non-Windows system or the user doesn't have Visual Studio installed
 MSBuild.exe UVKBuildTool.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
