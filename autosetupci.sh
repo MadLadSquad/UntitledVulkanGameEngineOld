8a435dae0a16d4cb9383becf8d99bad36341d00b
Stanislav Vasilev <contact@madman10k.online>
2022-03-07 07:55:54 +0200

    Update autosetupci.sh

--- autosetupci.sh
+++ autosetupci.sh
@@ -49,7 +49,8 @@ cp Release/UVKBuildToolLib.dll . || cp Release/libUVKBuildToolLib.dll . || echo 
 
 # Windows uses .exe files so the first instruction will fail on any non-Windows system
 ./UVKBuildTool.exe --install || ./UVKBuildTool --install
-echo "-------------------------------------------" && ls && exit
+echo "-------------------------------------------" && ls
+echo "-------------------------------------------" && ls ../../
 cd ../../ || exit
 mkdir build || exit # Build folder used for compilation
 mkdir Exported  || exit # Exported folder used for storing production ready games you exported
